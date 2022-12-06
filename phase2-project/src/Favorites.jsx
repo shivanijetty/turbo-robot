0d6fd5b95bd88dc30d080b840eca5a51843215ea
Cooper Hall <cooperhall111@gmail.com>
2022-12-06 14:19:50 -0500

    able to move recipe cards to favorites and can remove cards from favorites

--- phase2-project/src/Favorites.jsx
+++ phase2-project/src/Favorites.jsx
@@ -1,11 +1,29 @@
 
-const Favorites = ({favs}) => {
+const Favorites = ({favs, setFavs}) => {
+
+  const removeRecipes = (recipeCard) => {
+    console.log('bye bye recipe')
+    setFavs((prevState) => {
+    return([...prevState.filter((p)=> p.id !== recipeCard.id)])
+    })
+  }
+
   return(
       <div className="favorites">
         {
           favs.map((recps) => {
            return(
-              <img src={recps.image} />            
+              <div className="recipe card">
+                <img src={recps.image} className="card image" />
+                  <div className="small info">
+                     <h2>{recps.name}</h2>
+                     <p>{recps.description}</p>
+                     <button onClick={() => { removeRecipes(recps) }}>Remove</button>
+                  </div>
+                    {/* <div>
+                      <button onClick={() => display(recps)}>Modal Info!</button>
+                    </div> */}
+                  </div>        
             )
           })
         }
